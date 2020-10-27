f00318601050895e301bca1106ea40a1587559a2
Jaczina József <jacjozsmerinf@gmail.com>
2020-10-27 16:32:05 +0100

    Feladat megoldás
    A kiadott listázási feladat megoldása

---
+++ Controllers/RiportController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GAMF.Data;
+using GAMF.Models;
+using GAMF.Models.Views;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GAMF.Controllers
+{
+    public class RiportController : Controller
+    {
+        private readonly GAMFDbContext _context;
+
+        public RiportController(GAMFDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult StudentCredits()
+        {
+            //Nem szép de megy
+            IQueryable<StudentCreditsVM> credits =
+                from student in _context.Students
+                join enrollment in _context.Enrollments on student.Id equals enrollment.StudentId
+                join course in _context.Courses on enrollment.CourseId equals course.CourseId
+                select new StudentCreditsVM()
+                {
+                    StudentName = $"{student.LastName} {student.FirstMidName}",
+                    Credits = student.Enrollments.Where(e => e.Grade != Grade.F).Sum(e => e.Course.Credits),
+                    FullCredits = student.Enrollments.Sum(e => e.Course.Credits)
+                };
+            List<StudentCreditsVM> result = new List<StudentCreditsVM>();
+            foreach (var vm in credits.ToList().GroupBy(s => s.StudentName).OrderBy(s => s.Key))
+            {
+                result.Add(vm.First());
+            }
+            return View(result);
+        }
+    }
+}
---
+++ Models/Views/StudentCreditsVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GAMF.Models.Views
+{
+    public class StudentCreditsVM
+    {
+        [DisplayName("Hallgató neve")]
+        public string StudentName { get; set; }
+        [DisplayName("Teljesített kreditek száma")]
+        public int Credits { get; set; }
+        [DisplayName("Felvett kreditek száma")]
+        public int FullCredits { get; set; }
+    }
+}
